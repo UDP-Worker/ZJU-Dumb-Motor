c6d45a03f0199263ba4f98dfc18cd7602d761e23
Galath Potter <64718317+UDP-Worker@users.noreply.github.com>
2025-07-17 13:36:22 +0800

    Add minimal action queue test project

---
+++ Test-Motor/Core/Inc/motor.h
@@ -0,0 +1,28 @@
+#ifndef __MOTOR_H
+#define __MOTOR_H
+
+#include "stm32f1xx_hal.h"
+#include "tim.h"
+#include "gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    MOTOR_RIGHT = 0,
+    MOTOR_LEFT
+} Motor_Channel;
+
+void Motor_Init(void);
+void motor_fwd(void);
+void motor_bwd(void);
+void motor_turn_left(void);
+void motor_turn_right(void);
+void motor_brake(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MOTOR_H */
--- Test-Motor/Core/Src/stm32f1xx_it.c
+++ Test-Motor/Core/Src/stm32f1xx_it.c
@@ -22,6 +22,7 @@
 #include "stm32f1xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "queue.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -183,6 +184,7 @@ void PendSV_Handler(void)
 void SysTick_Handler(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
+  queue_tick();
 
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
