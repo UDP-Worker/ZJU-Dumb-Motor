41e1e7f5ffdb9071a5c070df0ce9543d40f3a499
Galath Potter <64718317+UDP-Worker@users.noreply.github.com>
2025-07-18 15:20:04 +0800

    Delete Dumb-Motor directory

--- Dumb-Motor/Core/Inc/tim.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIM_H
-#define __TIM_H
-
-#include "stm32f1xx_hal.h"
-
-extern TIM_HandleTypeDef htim2;
-
-#endif
--- Dumb-Motor/Core/Src/globals.c
+++
@@ -1,5 +0,0 @@
-#include "config.h"
-
-volatile uint8_t g_L = 0, g_R = 0;
-volatile uint16_t us_dist[21] = {0};
-volatile uint8_t us_idx = 0;
